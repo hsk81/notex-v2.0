cb3d99cad78bbe990887ce9c864c459c5dce255c
Hasan Karahan <hasan.karahan81@gmail.com>
2012-10-16 16:30:09 +0300

    Added assert function

---
+++ static/app/assert.js
@@ -0,0 +1,13 @@
+function AssertException (message) {
+    this.message = message;
+}
+
+AssertException.prototype.toString = function () {
+    return 'AssertException: ' + this.message;
+}
+
+function assert (expression, message) {
+    if (!expression) {
+        throw new AssertException (message);
+    }
+}
