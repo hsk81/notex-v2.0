790b66d6088bcd15244693fd248cbfe43d4e534c
Hasan Karahan <hasan.karahan81@gmail.com>
2012-10-16 17:25:35 +0300

    Getters for class and instance references

--- static/app/controller/SetTree.js
+++ static/app/controller/SetTree.js
@@ -1,6 +1,14 @@
 Ext.define ('Webed.controller.SetTree', {
     extend: 'Ext.app.Controller',
 
+    views: ['SetTree'],
+    models: ['Set', 'Doc', 'Set2Doc'],
+    stores: ['Sets', 'Docs', 'Set2Docs'],
+
+    refs: [{
+        selector: 'set-tree', ref: 'setTree' // getSetTree: view *instance*
+    }],
+
     init: function () {
         this.control ({
             // TODO: Wire view events!
@@ -13,5 +21,15 @@ Ext.define ('Webed.controller.SetTree', {
 
     synchronize: function () {
         console.debug ('[SetTreeCtrl.synchronize]', this);
+
+        var modelClass = this.getSetModel ();
+        assert (modelClass);
+        var storeInstance = this.getSetsStore ();
+        assert (storeInstance);
+
+        var viewClass = this.getSetTreeView ();
+        assert (viewClass);
+        var viewInstance = this.getSetTree ();
+        assert (viewInstance);
     }
 });
