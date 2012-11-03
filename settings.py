87378f3692e765c51ae635d85bfe07eb2b63f8bf
Hasan Karahan <hasan.karahan81@gmail.com>
2012-11-03 21:50:02 +0200

    Randomly generation SECRET_KEY on every run kills active sessions

--- settings.py
+++ settings.py
@@ -14,8 +14,6 @@ DEBUG = socket.gethostname () != SITE_HOST
 if not DEBUG:
     SERVER_NAME = '%s.%s' % (SITE_NAME, SITE_HOST)
 
-import base64
-SECRET_KEY = base64.encodestring (os.urandom (24))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/%s.db' % (SITE_ROOT, SITE_NAME)
 
 
