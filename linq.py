cc08cb9c9e1a9965c2f75d24625023e6d614ea40
Hasan Karahan <hasan.karahan81@gmail.com>
2012-11-05 13:29:01 +0200

    LINQ: First & last with defaults

--- linq.py
+++ linq.py
@@ -14,9 +14,32 @@ class Linq:
         assert len (self.iterable) > 0
         return self.iterable[0]
 
+    def first_or_default (self, default=None):
+
+        if len (self.iterable) > 0:
+            return self.iterable[0]
+        else:
+            return default
+
+    def last (self):
+
+        assert len (self.iterable) > 0
+        return self.iterable[-1]
+
+    def last_or_default (self, default=None):
+
+        if len (self.iterable) > 0:
+            return self.iterable[-1]
+        else:
+            return default
+
     def filter (self, fn):
 
         return Linq (filter (fn, self.iterable))
+
+    def select (self, fn):
+
+        pass ## TODO: Implement projection!
 
 ###############################################################################
 ###############################################################################
