2c5d356b9d8e21dcc3a70e55fa6aece92f2df958
Hasan Karahan <hasan.karahan81@gmail.com>
2012-11-10 19:47:49 +0200

    minor

---
+++ webed/config/default.py
@@ -0,0 +1,32 @@
+__author__ = 'hsk81'
+
+###############################################################################
+###############################################################################
+
+import os
+import socket
+
+from webed.util import in_rxs
+
+###############################################################################
+###############################################################################
+
+class DefaultConfig:
+
+    MACH_DEVS = [r'(.*)\.blackhan.ch$']
+    DEBUG = os.environ['DEBUG'] == 'True' if 'DEBUG' in os.environ\
+    else in_rxs (socket.getfqdn (), MACH_DEVS)
+
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/webed.db'
+
+    ##
+    ## If a set has more than LOADSKIP_LIMIT elements (sets plus docs), than
+    ## they are *not* preloaded. For LOADSKIP_LIMIT = 0, the elements of any
+    ## given set is loaded only on demand.
+    ##
+
+    LOADSKIP_LIMIT = 16
+
+###############################################################################
+###############################################################################
--- webed/config/test.py
+++ webed/config/test.py
@@ -3,29 +3,10 @@ __author__ = 'hsk81'
 ###############################################################################
 ###############################################################################
 
-import os
-import util
-import socket
+from webed.config import DefaultConfig
 
 ###############################################################################
 ###############################################################################
-
-class DefaultConfig:
-
-    MACH_DEVS = [r'(.*)\.blackhan.ch$']
-    DEBUG = os.environ['DEBUG'] == 'True' if 'DEBUG' in os.environ \
-        else util.in_rxs (socket.getfqdn (), MACH_DEVS)
-
-    SQLALCHEMY_ECHO = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/webed.db'
-
-    ##
-    ## If a set has more than LOADSKIP_LIMIT elements (sets plus docs), than
-    ## they are *not* preloaded. For LOADSKIP_LIMIT = 0, the elements of any
-    ## given set is loaded only on demand.
-    ##
-
-    LOADSKIP_LIMIT = 16
 
 class TestConfig (DefaultConfig):
 
