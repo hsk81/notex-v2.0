1edec3df9095f509603a34faf6ea1334e00660ab
Hasan Karahan <hasan.karahan81@gmail.com>
2012-11-12 12:53:46 +0200

    Dummy secret key for default and test env

--- webed/config/default.py
+++ webed/config/default.py
@@ -14,11 +14,18 @@ from webed.util import in_rxs
 class DefaultConfig:
 
     MACH_DEVS = [r'(.*)\.blackhan.ch$']
-    DEBUG = os.environ['DEBUG'] == 'True' if 'DEBUG' in os.environ\
-    else in_rxs (socket.getfqdn (), MACH_DEVS)
+    DEBUG = os.environ['DEBUG'] == 'True' if 'DEBUG' in os.environ \
+        else in_rxs (socket.getfqdn (), MACH_DEVS)
 
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/webed.db'
+
+    ##
+    ## Override secret key in production environment using value from the
+    ## *production.py* file.
+    ##
+
+    SECRET_KEY = '000000000000000000000000000000000000000='
 
     ##
     ## If a set has more than LOADSKIP_LIMIT elements (sets plus docs), than
