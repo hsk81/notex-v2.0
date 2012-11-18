9bd489403994e0473761e89f88da274e88ed0369
Hasan Karahan <hasan.karahan81@gmail.com>
2012-11-18 15:05:52 +0200

    Removed PIP deps and added clean-egg command

--- setup.sh
+++ setup.sh
@@ -7,37 +7,12 @@
 # Require $ sudo npm install -g grunt
 
 # Initial $ ./setup.sh init webed
-# Upgrade $ ./setup.sh init webed --upgrade
 
 ###############################################################################
 ###############################################################################
 
 ACTMETH=${1}
 APPNAME=${2-"webed"}
-PIPOPTS=${3-""}
-
-###############################################################################
-###############################################################################
-
-function pipit () {
-    pip install $1 Flask
-    pip install $1 Flask-SQLAlchemy
-    pip install $1 Flask-Admin
-    pip install $1 Flask-DebugToolbar
-    pip install $1 ipython
-}
-
-###############################################################################
-###############################################################################
-
-function vexec_pip () {
-    if [ -f bin/activate ] ; then
-        source bin/activate
-        if [ $VIRTUAL_ENV ] ; then
-            pipit $1 ; deactivate
-        fi
-    fi
-}
 
 ###############################################################################
 ###############################################################################
@@ -51,12 +26,16 @@ function setup_env () {
     fi
 }
 
-function clear_env () {
+function clean_env () {
     if [ $VIRTUAL_ENV ] ; then
         deactivate
     fi
 
     rm bin/ include/ lib/ -r
+}
+
+function clean_egg () {
+    rm build/ dist/ *.egg-info/ -r
 }
 
 function build () {
@@ -69,10 +48,14 @@ function build () {
 ###############################################################################
 
 case $ACTMETH in
-    clear)
-        clear_env ;;
+    clean)
+        clean_egg && clean_env ;;
+    clean-egg)
+        clean_egg ;;
+    clean-env)
+        clean_env ;;
     init)
-        setup_env $APPNAME ;; ## && vexec_pip $PIPOPTS ;;
+        setup_env $APPNAME ;;
     *)
         $0 init $1 $2 ;;
 esac
