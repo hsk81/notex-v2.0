74fe4ae834243c48695b1947512e66b96abcb38b
Hasan Karahan <hasan.karahan81@gmail.com>
2012-11-21 06:42:03 +0200

    minor

--- webed.py
+++ webed.py
@@ -6,7 +6,6 @@
 from webed.app import app
 from webed.ext import db
 from webed.models import User
-from webed.util.linq import Q
 
 from flask.ext.script import Manager
 
@@ -44,7 +43,7 @@ def drop ():
 @manager.command
 def execute (source):
     """Execute source in application context"""
-    with app.app_context(): exec source
+    with app.app_context (): exec source
 
 ###############################################################################
 ###############################################################################
