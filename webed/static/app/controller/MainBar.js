405c6527eeda3d1d069756495a1fd2557d6c6996
Hasan Karahan <hasan.karahan81@gmail.com>
2012-12-04 21:48:47 +0200

    Added confirmation/prompt dialog boxes to mainbar

--- webed/static/app/controller/MainBar.js
+++ webed/static/app/controller/MainBar.js
@@ -1,6 +1,16 @@
 Ext.define ('Webed.controller.MainBar', {
     extend: 'Ext.app.Controller',
 
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    refs: [{
+        selector: 'node-tree', ref: 'nodeTree'
+    }],
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
     init: function () {
         this.control ({
             'main-bar button[action=save-document]': {
@@ -36,45 +46,196 @@ Ext.define ('Webed.controller.MainBar', {
         });
     },
 
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
     saveDocument: function (item, event, options) {
-        console.debug ('[MainBarCtrl.saveDocument]');
+        console.debug ('[MainBar.saveDocument]');
     },
+
     openDocument: function (item, event, options) {
-        console.debug ('[MainBarCtrl.openDocument]');
+        console.debug ('[MainBar.openDocument]');
     },
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
     add: function (item, event, options) {
         this.addProject (item, event, options);
     },
+
     addProject: function (item, event, options) {
-        this.application.fireEvent ('create_node', {node: {
-            name: 'Project', mime: 'application/project',
-            root_uuid: '00000000-0000-0000-0000-000000000000'
-        }});
+        Ext.MessageBox.prompt (
+            'Add Project', 'Enter a name:', function (button, text) {
+                if (button != 'ok') return;
+
+                function callback (rec, op) {
+                    if (op.success) return;
+
+                    message.error ({ msg: Ext.String.format (
+                        message.CREATE_ERROR, text
+                    )});
+
+                    console.error ('[MainBar.addProject]', rec, op);
+                }
+
+                this.application.fireEvent ('create_node', {
+                    scope: this, callback: callback, node: {
+                        root_uuid: '00000000-0000-0000-0000-000000000000',
+                        mime: 'application/project',
+                        name: text
+                    }
+                });
+            }, this
+        );
     },
+
     addFolder: function (item, event, options) {
-        this.application.fireEvent ('create_node', {node: {
-            name: 'folder', mime: 'application/folder'
-        }});
+        Ext.MessageBox.prompt (
+            'Add Folder', 'Enter a name:', function (button, text) {
+                if (button != 'ok') return;
+
+                function callback (rec, op) {
+                    if (op.success) return;
+
+                    message.error ({ msg: Ext.String.format (
+                        message.CREATE_ERROR, text
+                    )});
+
+                    console.error ('[MainBar.addFolder]', rec, op);
+                }
+
+                this.application.fireEvent ('create_node', {
+                    scope: this, callback: callback, node: {
+                        mime: 'application/folder',
+                        name: text
+                    }
+                });
+            }, this
+        );
     },
+
     addText: function (item, event, options) {
-        this.application.fireEvent ('create_leaf', {leaf: {
-            name: 'options.cfg', mime: 'text/plain'
-        }});
+        Ext.MessageBox.prompt (
+            'Add Text', 'Enter a name:', function (button, text) {
+                if (button != 'ok') return;
+
+                function callback (rec, op) {
+                    if (op.success) return;
+
+                    message.error ({ msg: Ext.String.format (
+                        message.CREATE_ERROR, text
+                    )});
+
+                    console.error ('[MainBar.addText]', rec, op);
+                }
+
+                this.application.fireEvent ('create_leaf', {
+                    scope: this, callback: callback, leaf: {
+                        mime: 'text/plain',
+                        name: text
+                    }
+                });
+            }, this
+        );
     },
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
     rename: function (item, event, options) {
-        this.application.fireEvent ('update_node', {node: {
-            name: 're-named'
-        }});
+        var view = this.getNodeTree ();
+        assert (view);
+        var semo = view.getSelectionModel ();
+        assert (semo);
+        var node = semo.getLastSelected ();
+        assert (node);
+        var uuid = node.get ('uuid');
+        assert (uuid);
+
+        if (uuid == '00000000-0000-0000-0000-000000000000') {
+            return;
+        }
+
+        message.prompt ({ title: 'Rename', value: node.get ('name'),
+            scope: this, fn: function (button, text) {
+                if (button != 'ok') return;
+
+                function callback (rec, op) {
+                    if (op.success) return;
+
+                    message.error ({ msg: Ext.String.format (
+                        message.UPDATE_ERROR, node.get ('name')
+                    )});
+
+                    console.error ('[MainBar.rename]', rec, op);
+                }
+
+                this.application.fireEvent ('update_node', {
+                    scope: this, callback: callback, node: {
+                        uuid: uuid, name: text
+                    }
+                });
+            }
+        });
     },
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
     destroy: function (item, event, options) {
-        this.application.fireEvent ('delete_node', {node: {
-            uuid: null
-        }});
+        var view = this.getNodeTree ();
+        assert (view);
+        var semo = view.getSelectionModel ();
+        assert (semo);
+        var node = semo.getLastSelected ();
+        assert (node);
+        var uuid = node.get ('uuid');
+        assert (uuid);
+
+        if (uuid == '00000000-0000-0000-0000-000000000000') {
+            return;
+        }
+
+        message.confirm ({
+            title: 'Delete',
+            msg: Ext.String.format (
+                'Are you sure, do you want to delete <i>{0}</i>?',
+                node.get ('name')
+            ),
+            scope: this, fn: function (button) {
+                if (button != 'yes') return;
+
+                function callback (rec, op) {
+                    if (op.success) return;
+
+                    message.error ({ msg: Ext.String.format (
+                        message.DELETE_ERROR, node.get ('name')
+                    )});
+
+                    console.error ('[MainBar.destroy]', rec, op);
+                }
+
+                this.application.fireEvent ('delete_node', {
+                    scope: this, callback: callback, node: {
+                        uuid: uuid
+                    }
+                });
+            }
+        });
     },
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
     importProject: function (item, event, options) {
-        console.debug ('[MainBarCtrl.importProject]');
+        console.debug ('[MainBar.importProject]');
     },
+
     exportProject: function (item, event, options) {
-        console.debug ('[MainBarCtrl.exportProject]');
+        console.debug ('[MainBar.exportProject]');
     }
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
 });
