ab982bb847982b5462815869d697dce7a356ce35
Hasan Karahan <hasan.karahan81@gmail.com>
2012-12-05 17:58:22 +0200

    minor

--- webed/static/app/controller/MainBar.js
+++ webed/static/app/controller/MainBar.js
@@ -197,8 +197,9 @@ Ext.define ('Webed.controller.MainBar', {
         var uuid = node.get ('uuid');
         assert (uuid);
 
-        if (uuid == '00000000-0000-0000-0000-000000000000')
+        if (uuid == '00000000-0000-0000-0000-000000000000') {
             return;
+        }
 
         message.confirm ({
             title: 'Delete',
