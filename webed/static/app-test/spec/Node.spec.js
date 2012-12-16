d72376cc6c4b61c9d9fa9ee8c3408b4c21598541
Hasan Karahan <hasan.karahan81@gmail.com>
2012-12-16 15:20:48 +0500

    Added Node controller with test spec

---
+++ webed/static/app-test/spec/Node.spec.js
@@ -0,0 +1,125 @@
+describe ('NodeController', function () {
+    Ext.require('Webed.controller.Node');
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    var lock = function () {
+        var list = []; return {
+            empty: function () { return list.length == 0; },
+            push: function (el) { list.push (el); },
+            pop: function () { return list.pop (); },
+            clear: function () { list = []; },
+            init: function () { list = [true]; }
+        }
+    }();
+
+    var controller = null;
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    beforeEach (function () {
+        if (!controller) controller = window.app.getController ('Node');
+        expect (controller).toBeTruthy (); controller.init ();
+        lock.init (); // ensures that callback expectations are met!
+    });
+
+    afterEach (function () {
+        controller = null;
+        lock.clear ();
+
+        var reset = null; Ext.Ajax.request ({
+            url: '/reset/', callback: function (opt, success, xhr) {
+                expect (success).toBeTruthy ();
+                reset = success;
+            }
+        });
+
+        waitsFor (function () { return reset; }, 'reset', 500);
+    });
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    it ('should set a node', function () {
+        var nodes = window.app.getStore ('Nodes');
+        expect (nodes).toBeTruthy ();
+
+        nodes.load ({scope: this, callback: function (records, op, success) {
+            expect (records).toBeTruthy ();
+            expect (records.length).toBeGreaterThan (0);
+
+            var root = records[0];
+            expect (root).toBeTruthy ();
+            var uuid = root.get ('uuid');
+            expect (uuid).toBeTruthy ();
+
+            window.app.fireEvent ('set_node', this, {
+                scope: this, callback: on_set, node: [{
+                    root_uuid: uuid,
+                    name: 'node',
+                    mime: 'plain/text'
+                },{
+                    root_uuid: uuid,
+                    name: 'node',
+                    mime: 'plain/text'
+                }]
+            });
+
+            function on_set (node, op, index) {
+                expect (node).toBeTruthy ();
+                expect (node.get ('root_uuid')).toEqual (uuid);
+                expect (node.get ('name')).toEqual ('node');
+                expect (node.get ('mime')).toEqual ('plain/text');
+                expect (op).toBeTruthy ();
+                expect (op.success).toBeTruthy ();
+                if (index+1>=2) lock.pop ();
+            }
+        }});
+
+        waitsFor (function () { return lock.empty (); }, 'unlock', 500);
+    });
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    it ('should get a node', function () {
+        var nodes = window.app.getStore ('Nodes');
+        expect (nodes).toBeTruthy ();
+
+        nodes.load ({scope: this, callback: function (records, op, success) {
+            expect (records).toBeTruthy ();
+            expect (records.length).toBeGreaterThan (0);
+
+            var root = records[0];
+            expect (root).toBeTruthy ();
+            var uuid = root.get ('uuid');
+            expect (uuid).toBeTruthy ();
+
+            window.app.fireEvent ('get_node', this, {
+                scope: this, callback: on_get, node: [{
+                    root_uuid: uuid
+                },{
+                    root_uuid: uuid
+                }]
+            });
+
+            function on_get (nodes, op, index) {
+                expect (nodes).toBeTruthy ();
+                expect (nodes.length).toBeGreaterThan (0);
+                expect (nodes.length).toBeGreaterThan (index);
+                expect (nodes[index]).toBeTruthy ();
+                expect (nodes[index].get ('root_uuid')).toEqual (uuid);
+                expect (op).toBeTruthy ();
+                expect (op.success).toBeTruthy ();
+                if (index+1>=nodes.length) lock.pop ();
+            }
+        }});
+
+        waitsFor (function () { return lock.empty (); }, 'unlock', 500);
+    });
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+});
---
+++ webed/static/app/controller/Node.js
@@ -0,0 +1,81 @@
+Ext.define ('Webed.controller.Node', {
+    extend: 'Ext.app.Controller',
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    models: ['Node'],
+    stores: ['Nodes'],
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    init: function () {
+        this.application.on ({
+            set_node: this.set_node, scope: this
+        });
+
+        this.application.on ({
+            get_node: this.get_node, scope: this
+        });
+    },
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    set_node: function (source, args) {
+        if (source == this) return;
+
+        assert (args);
+        assert (args.node);
+        assert (args.node.length >= 0);
+
+        for (var index in args.node) {
+            var node = args.node[index];
+
+            assert (node);
+            assert (node.root_uuid);
+            assert (node.uuid||true);
+            assert (node.mime);
+            assert (node.name);
+
+            var model = Ext.create ('Webed.model.Node', node);
+            assert (model);
+
+            var model = model.save ({
+                scope: args.scope||this, callback: function (rec, op) {
+                    if (args.callback && args.callback.call) {
+                        args.callback.call (args.scope||this, rec, op, index);
+                    }
+                }
+            });
+
+            assert (model);
+        }
+    },
+
+    get_node: function (source, args) {
+        if (source == this) return;
+
+        assert (args);
+        assert (args.node);
+        assert (args.node.length >= 0);
+        assert (args.callback);
+
+        for (var index in args.node) {
+            var node = args.node[index];
+            assert (node);
+            var store = this.getNodesStore ();
+            assert (store);
+
+            store.load ({
+                scope: args.scope||this, callback: function (recs, op) {
+                    args.callback.call (args.scope||this, recs, op, index);
+                }, params: node
+            });
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+});
