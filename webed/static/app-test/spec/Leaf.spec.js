fee48c9534520622ac00e87fdf1bc8589a9f5e1a
Hasan Karahan <hasan.karahan81@gmail.com>
2012-12-16 16:29:14 +0500

    Leaf controller with spec

---
+++ webed/static/app-test/spec/Leaf.spec.js
@@ -0,0 +1,124 @@
+describe ('LeafController', function () {
+    Ext.require('Webed.controller.Leaf');
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    var controller = null, lock = function () {
+        var list = []; return {
+            init: function (ls) { list = ls||[]; },
+            empty: function () { return list.length == 0; },
+            clear: function () { list = []; },
+            push: function (el) { list.push (el); },
+            pop: function () { return list.pop (); }
+        }
+    }();
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    beforeEach (function () {
+        if (!controller) controller = window.app.getController ('Leaf');
+        expect (controller).toBeTruthy (); controller.init ();
+    });
+
+    afterEach (function () {
+        controller = null;
+
+        lock.init ([true]); Ext.Ajax.request ({
+            url: '/reset/', callback: function (opt, success, xhr) {
+                expect (success).toBeTruthy (); lock.pop ();
+            }
+        });
+
+        waitsFor (function () { return lock.empty (); }, 'reset', 500);
+    });
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    it ('should set leafs', function () {
+        var nodes = window.app.getStore ('Nodes');
+        expect (nodes).toBeTruthy ();
+
+        lock.init ([true, true]); // ensure callback verification!
+
+        nodes.load ({scope: this, callback: function (records, op, success) {
+            expect (records).toBeTruthy ();
+            expect (records.length).toBeGreaterThan (0);
+
+            var root = records[0];
+            expect (root).toBeTruthy ();
+            var uuid = root.get ('uuid');
+            expect (uuid).toBeTruthy ();
+
+            window.app.fireEvent ('set_leaf', this, {
+                scope: this, callback: on_set, leaf: [{
+                    root_uuid: uuid,
+                    name: 'leaf',
+                    mime: 'plain/text'
+                },{
+                    root_uuid: uuid,
+                    name: 'leaf',
+                    mime: 'plain/text'
+                }]
+            });
+
+            function on_set (leaf, op, index) {
+                expect (leaf).toBeTruthy ();
+                expect (leaf.get ('root_uuid')).toEqual (uuid);
+                expect (leaf.get ('name')).toEqual ('leaf');
+                expect (leaf.get ('mime')).toEqual ('plain/text');
+                expect (op).toBeTruthy ();
+                expect (op.success).toBeTruthy ();
+                lock.pop ();
+            }
+        }});
+
+        waitsFor (function () { return lock.empty (); }, 'unlock', 500);
+    });
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    it ('should get leafs', function () {
+        var nodes = window.app.getStore ('Nodes');
+        expect (nodes).toBeTruthy ();
+
+        lock.init ([true, true]); // ensure callback verification!
+
+        nodes.load ({scope: this, callback: function (records, op, success) {
+            expect (records).toBeTruthy ();
+            expect (records.length).toBeGreaterThan (0);
+
+            var root = records[0];
+            expect (root).toBeTruthy ();
+            var uuid = root.get ('uuid');
+            expect (uuid).toBeTruthy ();
+
+            window.app.fireEvent ('get_leaf', this, {
+                scope: this, callback: on_get, leaf: [{
+                    root_uuid: uuid
+                },{
+                    root_uuid: uuid
+                }]
+            });
+
+            function on_get (leafs, op, index) {
+                expect (leafs).toBeTruthy ();
+                expect (leafs.length).toBeGreaterThan (0);
+                expect (leafs.length).toBeGreaterThan (index);
+                expect (leafs[index]).toBeTruthy ();
+                expect (leafs[index].get ('root_uuid')).toEqual (uuid);
+                expect (op).toBeTruthy ();
+                expect (op.success).toBeTruthy ();
+                lock.pop ();
+            }
+        }});
+
+        waitsFor (function () { return lock.empty (); }, 'unlock', 500);
+    });
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+});
---
+++ webed/static/app/controller/Leaf.js
@@ -0,0 +1,81 @@
+Ext.define ('Webed.controller.Leaf', {
+    extend: 'Ext.app.Controller',
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    models: ['Leaf'],
+    stores: ['Leafs'],
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    init: function () {
+        this.application.on ({
+            set_leaf: this.set_leaf, scope: this
+        });
+
+        this.application.on ({
+            get_leaf: this.get_leaf, scope: this
+        });
+    },
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    set_leaf: function (source, args) {
+        if (source == this) return;
+
+        assert (args);
+        assert (args.leaf);
+        assert (args.leaf.length >= 0);
+
+        for (var index in args.leaf) {
+            var leaf = args.leaf[index];
+
+            assert (leaf);
+            assert (leaf.root_uuid);
+            assert (leaf.uuid||true);
+            assert (leaf.mime);
+            assert (leaf.name);
+
+            var model = Ext.create ('Webed.model.Leaf', leaf);
+            assert (model);
+
+            var model = model.save ({
+                scope: args.scope||this, callback: function (rec, op) {
+                    if (args.callback && args.callback.call) {
+                        args.callback.call (args.scope||this, rec, op, index);
+                    }
+                }
+            });
+
+            assert (model);
+        }
+    },
+
+    get_leaf: function (source, args) {
+        if (source == this) return;
+
+        assert (args);
+        assert (args.leaf);
+        assert (args.leaf.length >= 0);
+        assert (args.callback);
+
+        for (var index in args.leaf) {
+            var leaf = args.leaf[index];
+            assert (leaf);
+            var store = this.getLeafsStore ();
+            assert (store);
+
+            store.load ({
+                scope: args.scope||this, callback: function (recs, op) {
+                    args.callback.call (args.scope||this, recs, op, index);
+                }, params: leaf
+            });
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+});
