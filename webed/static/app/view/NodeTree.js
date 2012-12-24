07918d287546323d02462bc85f41b47100f0ebc7
Hasan Karahan <hasan.karahan81@gmail.com>
2012-12-24 20:59:49 +0500

    minor

--- webed/static/app/view/NodeTree.js
+++ webed/static/app/view/NodeTree.js
@@ -8,6 +8,7 @@ Ext.define ('Webed.view.NodeTree', {
         'Ext.grid.column.Template'
     ],
 
+    animate: true,
     rootVisible: false,
     hideHeaders: false,
     fields: ['name', 'size'],
