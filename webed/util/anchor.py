d3a3279364b5442719d832ef029d7361d16bd781
Hasan Karahan <hasan.karahan81@gmail.com>
2012-12-31 21:11:15 +0500

    minor fixes in Anchor class

--- webed/util/anchor.py
+++ webed/util/anchor.py
@@ -21,21 +21,21 @@ class Anchor (object):
         version_key = key if key else self.get_version_key ()
         return (cache.get (version_key) or 0) if version_key else 0
 
-    def get_value_key (self, version_key=None):
-        version = self.get_version (key=version_key)
+    def get_value_key (self, version=None, version_key=None):
+        version = version if version else self.get_version (key=version_key)
         return cache.make_key (self.session['_id'], 'anchor', version)
 
-    def get_value (self, key=None):
-        value_key = key if key else self.get_value_key ()
+    def get_value (self, key=None, version_key=None):
+        value_key = key if key else self.get_value_key (version_key=version_key)
         return cache.get (value_key) if value_key else None
 
-    def set_value (self, value, key=None):
-        value_key = key if key else self.get_value_key ()
+    def set_value (self, value, key=None, version_key=None):
+        value_key = key if key else self.get_value_key (version_key=version_key)
         if value_key: cache.set (value_key, value, timeout=self.timeout)
 
     def reset (self):
         version_key = self.get_version_key ()
-        version = self.get_version(key=version_key)
+        version = self.get_version (key=version_key)
         cache.set (version_key, version+1, timeout=0) ## indefinite
 
     def refresh (self):
@@ -52,6 +52,10 @@ class Anchor (object):
     @property
     def initiated (self):
         return self.get_value () is not None
+
+    @property
+    def value (self):
+        return self.get_value ()
 
 ###############################################################################
 ###############################################################################
