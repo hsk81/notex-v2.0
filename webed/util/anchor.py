0f23beb48770d8c298434198594646432f2cfe8f
Hasan Karahan <hasan.karahan81@gmail.com>
2013-01-01 21:09:24 +0500

    Simplified anchor implementation

--- webed/util/anchor.py
+++ webed/util/anchor.py
@@ -11,36 +11,41 @@ from ..ext.cache import cache
 class Anchor (object):
 
     def __init__ (self, session):
-        self.session = session
+
+        self.sid = session['_id']
+
+    ###########################################################################
 
     def get_version_key (self):
         return cache.make_key ('global', 'version', 'anchor')
 
-    def get_version (self, key=None):
-        version_key = key if key else self.get_version_key ()
-        return cache.get (version_key) or 0
+    def get_version (self):
+        key = self.get_version_key ()
+        return cache.get (key) or 0, key
 
-    def get_value_key (self, version=None, version_key=None):
-        version = version if version else self.get_version (key=version_key)
-        return cache.make_key (self.session['_id'], 'anchor', version)
+    ###########################################################################
 
-    def get_value (self, key=None, version_key=None):
-        value_key = key if key else self.get_value_key (version_key=version_key)
-        return cache.get (value_key)
+    def get_value_key (self):
+        version, _ = self.get_version ()
+        return cache.make_key (self.sid, 'anchor', version), version
 
-    def set_value (self, value, timeout=None, key=None, version_key=None):
-        value_key = key if key else self.get_value_key (version_key=version_key)
-        cache.set (value_key, value, timeout=timeout)
+    def get_value (self):
+        key, _ = self.get_value_key ()
+        return cache.get (key), key
+
+    def set_value (self, value, timeout=None):
+        key, _ = self.get_value_key ()
+        cache.set (key, value, timeout=timeout)
+
+    ###########################################################################
 
     def reset (self):
-        version_key = self.get_version_key ()
-        version = self.get_version (key=version_key)
-        cache.set (version_key, version+1, timeout=0) ## indefinite
+        version, key = self.get_version ()
+        cache.set (key, version+1, timeout=0) ## indefinite
 
     def refresh (self):
-        value_key = self.get_value_key ()
-        value = self.get_value (key=value_key)
-        if value: cache.delete (value_key)
+        value, key = self.get_value ()
+        if value: cache.delete (key)
 
      ## if value: ## TODO: Queue delete task!
      ##     base = Q (Node.query).one_or_default (uuid=value)
@@ -48,13 +53,28 @@ class Anchor (object):
      ##         db.session.delete (base)
      ##         db.session.commit ()
 
+    ###########################################################################
+
     @property
-    def initiated (self):
-        return self.get_value () is not None
+    def key (self):
+        key, _ = self.get_value_key ()
+        return key
 
     @property
     def value (self):
-        return self.get_value ()
+        value, _ = self.get_value ()
+        return value
+
+    @property
+    def version (self):
+        version, _ = self.get_version ()
+        return version
+
+    ###########################################################################
+
+    @property
+    def initiated (self):
+        return self.value is not None
 
 ###############################################################################
 ###############################################################################
