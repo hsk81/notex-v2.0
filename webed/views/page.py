9b364d451a1aaeeed34349e7fed40ba6fd80464f
Hasan Karahan <hasan.karahan81@gmail.com>
2013-01-03 16:48:03 +0500

    minor

--- webed/views/page.py
+++ webed/views/page.py
@@ -21,6 +21,10 @@ page = Blueprint ('page', __name__)
 ###############################################################################
 ###############################################################################
 
+@page.route ('/test/')
+def test ():
+    return main (page='home', template='index_test.html')
+
 @page.route ('/home/')
 def home (): return main (page='home')
 @page.route ('/overview/')
@@ -31,10 +35,6 @@ def tutorial (): return main (page='tutorial')
 def faq (): return main (page='faq')
 @page.route ('/contact/')
 def contact (): return main (page='contact')
-
-@page.route ('/test/')
-def test ():
-    return main (page='home', template='index_test.html')
 
 @page.route ('/')
 def main (page='home', template='index.html'):
