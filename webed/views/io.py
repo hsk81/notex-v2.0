dcb2e0c2a9af951194f020980e595ace03e5b37a
Hasan Karahan <hasan.karahan81@gmail.com>
2013-01-05 16:52:10 +0500

    Implemented `archive-upload` though there are still some issue left: `guess-mime` works partially (use better detector based on actual content); and although the properties seem to be created correctly at import, there connection to their corresponding leafs seem to be missing; fix!

--- webed/views/io.py
+++ webed/views/io.py
@@ -14,6 +14,13 @@ from ..util import JSON, Q
 from ..models import Node, Leaf
 from ..models import TextProperty, LargeBinaryProperty
 
+import mimetypes
+import tempfile
+import zipfile
+import urllib
+import shutil
+import os
+
 ###############################################################################
 ###############################################################################
 
@@ -66,7 +73,121 @@ def file_upload ():
 
 @io.route ('/archive-upload/', methods=['POST'])
 def archive_upload ():
-    return file_upload () ## TODO!
+    file = request.files['file']
+
+    if not file:
+        return JSON.encode (dict (success=False, filename=None,
+            message='file expected'))
+
+    if not file.filename or len (file.filename) == 0:
+        return JSON.encode (dict (success=False, filename=None,
+            message='filename invalid'))
+
+    with tempfile.TemporaryFile () as zip_file:
+
+        file.save (zip_file)
+        zip_file.flush ()
+
+        if not zipfile.is_zipfile (zip_file):
+            return JSON.encode (dict (success=False, filename=file.filename,
+                message='ZIP format expected'))
+
+        zip_name = secure_filename (file.filename)
+        zip_name = os.path.splitext (zip_name)[0]
+
+        temp_path = tempfile.mkdtemp  ()
+        extract (zip_file, path=temp_path)
+        create_prj (path=temp_path, name=zip_name)
+        shutil.rmtree (temp_path)
+
+    return JSON.encode (dict (success=True, filename=file.filename))
+
+###############################################################################
+
+def extract (zip_file, path):
+
+    def sanitize (path):
+
+        path = urllib.unquote_plus (path)
+        path = os.path.join (os.sep, path)
+        path = os.path.abspath (path)
+        path = path.lstrip (os.sep)
+
+        return path
+
+    with zipfile.ZipFile (zip_file, 'r') as zip_buffer:
+        for zi in zip_buffer.infolist ():
+
+            zi.filename = sanitize (zi.filename)
+            if not zi.filename: continue ## security!
+            zip_buffer.extract (zi, path=path)
+
+###############################################################################
+
+def create_prj (path, name):
+
+    base = Q (Node.query).one (uuid=app.session_manager.anchor)
+    assert base
+
+    prj_node = Node (name, base, mime='application/project')
+    cache = {path: prj_node}
+    db.session.add (prj_node)
+
+    for cur_path, dir_names, file_names in os.walk (path):
+        root = cache.get (cur_path)
+        assert root
+
+        for dn in dir_names:
+            node = create_dir (cur_path, dn, root, mime='application/folder')
+            dir_path = os.path.join (cur_path, dn)
+            cache[dir_path] = node
+            db.session.add (node)
+
+        for fn in file_names:
+            mime = guess_mime (cur_path, fn)
+            if mime == 'text/plain':
+                leaf, prop = create_txt (cur_path, fn, root, mime=mime)
+            else:
+                leaf, prop = create_bin (cur_path, fn, root, mime=mime)
+            db.session.add (leaf)
+            db.session.add (prop)
+
+    db.session.commit ()
+    return prj_node
+
+def create_dir (path, name, root, mime):
+
+    return Node (name, root, mime=mime)
+
+def create_txt (path, name, root, mime):
+
+    with open (os.path.join (path, name)) as file:
+        data = file.read ().replace ('\r\n','\n')
+
+    leaf = Leaf (name, root, mime=mime)
+    prop = TextProperty (name, data, leaf, mime=mime)
+
+    return leaf, prop
+
+def create_bin (path, name, root, mime):
+
+    with open (os.path.join (path, name)) as file:
+        data = file.read () ## TODO: 'data:%s;base64,..'?
+
+    leaf = Leaf (name, root, mime=mime)
+    prop = LargeBinaryProperty (name, data, leaf, mime=mime)
+
+    return leaf, prop
+
+###############################################################################
+
+def guess_mime (path, name):
+
+    if not mimetypes.inited:
+        mimetypes.init ()
+
+    mimetype, _ = mimetypes.guess_type (name)
+    return mimetype
 
 ###############################################################################
 ###############################################################################
