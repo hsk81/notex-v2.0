bbda864e006a9dbb1914b29bd365f002b13ea40b
Hasan Karahan <hasan.karahan81@gmail.com>
2013-01-14 21:46:00 +0500

    minor

--- webed/static/app/controller/MainBar.js
+++ webed/static/app/controller/MainBar.js
@@ -304,7 +304,6 @@ Ext.define ('Webed.controller.MainBar', {
         var uuid = node.get ('uuid'); assert (uuid);
         var url = '/archive-download/?node_uuid=' + uuid;
 
-
         Ext.Ajax.request ({
             url: url, scope: this, callback: function (opts, status, xhr) {
                 if (status) {
