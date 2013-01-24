259b49ffcba7da5f9285c9a2e5191712fe52f5a8
Hasan Karahan <hasan.karahan81@gmail.com>
2013-01-24 23:07:27 +0500

    Fix: Using {node,leaf}.{uuid,name}_path kills the results (for some unknown reason); keep {node,leaf}.get_path (field='..')

--- webed/views/rest.py
+++ webed/views/rest.py
@@ -485,8 +485,8 @@ def node2ext (node, leafs=True, level=1):
             'results': results,
             'root_uuid': node.root.uuid if node.root else None,
             'uuid': node.uuid,
-            'uuid_path': node.uuid_path,
-            'name_path': node.name_path,
+            'uuid_path': node.get_path (field='uuid'),
+            'name_path': node.get_path (field='name')
         }
 
     if app.config['MAX_NODE_SIZE'] < node.nodes.count ():
@@ -522,8 +522,8 @@ def leaf2ext (leaf):
         'results': None,
         'root_uuid': leaf.root.uuid,
         'uuid': leaf.uuid,
-        'uuid_path': leaf.uuid_path,
-        'name_path': leaf.name_path
+        'uuid_path': leaf.get_path (field='uuid'),
+        'name_path': leaf.get_path (field='name')
     }
 
 def prop2ext (prop):
