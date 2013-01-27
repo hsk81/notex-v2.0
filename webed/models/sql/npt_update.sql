4efdba3dbad6fecf0bd7478a87a02a7b38c0154f
Hasan Karahan <hasan.karahan81@gmail.com>
2013-01-27 15:04:20 +0500

    Removed slow `npt_update` functions

--- webed/models/sql/npt_update.sql
+++ webed/models/sql/npt_update.sql
@@ -1,23 +1,4 @@
 ﻿-------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION npt_update_node (IN nid integer)
-  RETURNS void LANGUAGE plpgsql VOLATILE AS
-$BODY$
-  BEGIN
-UPDATE ONLY node_path npt
-   SET base_id = npv.base_id,
-       node_id = npv.node_id,
-       uuid_path = array_to_string (npv.uuid_path, '/'),
-       name_path = array_to_string (npv.name_path, '/'),
-       dirty = FALSE
-  FROM node_path_view npv
- WHERE npt.node_id = nid AND npv.node_id = nid
-   AND npt.dirty = TRUE;
-   END
-$BODY$;
-
-ALTER FUNCTION public.npt_update_node (IN nid integer)
-  OWNER TO webed;
-
 CREATE OR REPLACE FUNCTION npt_cancel_node (IN nid integer)
   RETURNS void LANGUAGE plpgsql VOLATILE AS
 $BODY$
@@ -32,26 +13,6 @@ ALTER FUNCTION public.npt_cancel_node (IN nid integer)
   OWNER TO webed;
 
 -------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION npt_update_base (IN bid integer)
-  RETURNS void LANGUAGE plpgsql VOLATILE AS
-$BODY$
- BEGIN
-UPDATE ONLY node_path npt
-   SET base_id = npv.base_id,
-       node_id = npv.node_id,
-       uuid_path = array_to_string (npv.uuid_path, '/'),
-       name_path = array_to_string (npv.name_path, '/'),
-       dirty = FALSE
-  FROM node_path_view npv
- WHERE npt.base_id = bid AND npv.base_id = bid
-   AND npt.node_id = npv.node_id
-   AND npt.dirty = TRUE;
-   END
-$BODY$;
-
-ALTER FUNCTION public.npt_update_base (IN bid integer)
-  OWNER TO webed;
-
 CREATE OR REPLACE FUNCTION npt_cancel_base (IN bid integer)
   RETURNS void LANGUAGE plpgsql VOLATILE AS
 $BODY$
@@ -66,25 +27,6 @@ ALTER FUNCTION public.npt_cancel_base (IN bid integer)
   OWNER TO webed;
 
 -------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION npt_update_full ()
-  RETURNS void LANGUAGE plpgsql VOLATILE AS
-$BODY$
- BEGIN
-UPDATE ONLY node_path npt
-   SET base_id = npv.base_id,
-       node_id = npv.node_id,
-       uuid_path = array_to_string (npv.uuid_path, '/'),
-       name_path = array_to_string (npv.name_path, '/'),
-       dirty = FALSE
-  FROM node_path_view npv
- WHERE npt.node_id = npv.node_id
-   AND npt.dirty = TRUE;
-   END
-$BODY$;
-
-ALTER FUNCTION public.npt_update_full ()
-  OWNER TO webed;
-
 CREATE OR REPLACE FUNCTION npt_cancel_full ()
   RETURNS void LANGUAGE plpgsql VOLATILE AS
 $BODY$
