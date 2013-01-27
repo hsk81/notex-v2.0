01f519e1d951f7a33ca29eebed6ffbb453b3937b
Hasan Karahan <hasan.karahan81@gmail.com>
2013-01-27 13:37:49 +0500

    Materialized view `node_path`: Delete functions.

---
+++ webed/models/sql/npt_delete.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE FUNCTION npt_delete_node (IN nid integer)
+  RETURNS void LANGUAGE plpgsql VOLATILE AS
+$BODY$
+ BEGIN
+DELETE
+  FROM node_path npt
+ WHERE npt.node_id = nid;
+   END
+$BODY$;
+
+ALTER FUNCTION public.npt_delete_node (IN bid integer)
+  OWNER TO webed;
+
+CREATE OR REPLACE FUNCTION npt_delete_base (IN bid integer)
+  RETURNS void LANGUAGE plpgsql VOLATILE AS
+$BODY$
+ BEGIN
+DELETE
+  FROM node_path npt
+ WHERE npt.base_id = bid;
+   END
+$BODY$;
+
+ALTER FUNCTION public.npt_delete_base (IN bid integer)
+  OWNER TO webed;
+
+CREATE OR REPLACE FUNCTION npt_delete_full ()
+  RETURNS void LANGUAGE plpgsql VOLATILE AS
+$BODY$
+ BEGIN
+DELETE
+  FROM node_path npt;
+   END
+$BODY$;
+
+ALTER FUNCTION public.npt_delete_full ()
+  OWNER TO webed;
