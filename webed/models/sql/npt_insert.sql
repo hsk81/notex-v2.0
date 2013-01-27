d09140665010acdd8aeb645f3444895497063ab2
Hasan Karahan <hasan.karahan81@gmail.com>
2013-01-27 13:11:29 +0500

    Materialized view `node_path`: Refined *refresh* (now *update*) and *insert* functions.

--- webed/models/sql/npt_insert.sql
+++ webed/models/sql/npt_insert.sql
@@ -33,3 +33,20 @@ $BODY$;
 
 ALTER FUNCTION public.npt_insert_base (IN bid integer)
   OWNER TO webed;
+
+CREATE OR REPLACE FUNCTION npt_insert_full ()
+  RETURNS void LANGUAGE plpgsql VOLATILE AS
+$BODY$
+ BEGIN
+INSERT INTO node_path
+SELECT npv.base_id AS base_id,
+       npv.node_id AS node_id,
+       array_to_string (npv.uuid_path, '/') AS uuid_path,
+       array_to_string (npv.name_path, '/') AS name_path,
+       FALSE AS dirty
+  FROM node_path_view npv;
+   END
+$BODY$;
+
+ALTER FUNCTION public.npt_insert_full ()
+  OWNER TO webed;
