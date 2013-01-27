177ae4d34fd960c4189f21b937a0137e14c673c0
Hasan Karahan <hasan.karahan81@gmail.com>
2013-01-27 13:37:55 +0500

    minor

--- webed/models/sql/npv_create.sql
+++ webed/models/sql/npv_create.sql
@@ -1,28 +1,31 @@
 CREATE OR REPLACE VIEW node_path_view AS
 
-	WITH RECURSIVE graph (id, root_id, base_id, id_path, uuid_path, name_path) AS (
-	    SELECT n.id,
-	           COALESCE (n.root_id, n.id),
-	           COALESCE (n.base_id, n.id),
-	           ARRAY[n.id],
-	           ARRAY[n.uuid],
-	           ARRAY[n.name]
-	    FROM node n
+WITH RECURSIVE graph (id, root_id, base_id, id_path, uuid_path, name_path) AS (
+    SELECT n.id,
+           COALESCE (n.root_id, n.id),
+           COALESCE (n.base_id, n.id),
+           ARRAY[n.id],
+           ARRAY[n.uuid],
+           ARRAY[n.name]
+      FROM node n
+UNION
+    SELECT n.id,
+           COALESCE (n.root_id, n.id),
+           COALESCE (n.base_id, n.id),
+           id_path||ARRAY[n.id],
+           uuid_path||ARRAY[n.uuid],
+           name_path||ARRAY[n.name]
+      FROM node n, graph g
+     WHERE (n.root_id = g.id))
 
-	UNION
-	    SELECT n.id,
-	           COALESCE (n.root_id, n.id),
-	           COALESCE (n.base_id, n.id),
-	           id_path||ARRAY[n.id],
-	           uuid_path||ARRAY[n.uuid],
-	           name_path||ARRAY[n.name]
-	    FROM node n, graph g
-	    WHERE (n.root_id = g.id))
-
-	SELECT g.id AS node_id, g.root_id, g.base_id, id_path,
-	       g.uuid_path, g.name_path
-	FROM graph g
-	WHERE g.base_id = g.id_path[1];
+SELECT g.id AS node_id,
+       g.root_id,
+       g.base_id,
+       id_path,
+       g.uuid_path,
+       g.name_path
+  FROM graph g
+ WHERE g.base_id = g.id_path[1];
 
 ALTER VIEW node_path_view
 OWNER TO webed;
