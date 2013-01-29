80c5087313951452984005f86593253a213699cc
Hasan Karahan <hasan.karahan81@gmail.com>
2013-01-29 14:30:05 +0500

    Propert test DB set-up and tear-down.

--- webed/test/base.py
+++ webed/test/base.py
@@ -17,14 +17,21 @@ class BaseTestCase (unittest.TestCase):
     def setUp (self):
 
         app.config.from_object (TestConfig)
-
         self.app = app.test_client ()
+
         self.db = db
         self.db.create_all ()
+        self.db.session.script (['webed', 'models', 'sql', 'npv_create.sql'])
+        self.db.session.script (['webed', 'models', 'sql', 'npt_insert.sql'])
+        self.db.session.script (['webed', 'models', 'sql', 'npt_delete.sql'])
+        self.db.session.commit ()
 
     def tearDown (self):
 
         self.db.session.remove ()
+        self.db.session.script (['webed', 'models', 'sql', 'npv_drop.sql'])
+        self.db.session.script (['webed', 'models', 'sql', 'npt_drop.sql'])
+        self.db.session.commit ()
         self.db.drop_all ()
 
 ###############################################################################
