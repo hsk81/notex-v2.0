666da0acb8d0eb4c31e650763993ed7faf9e073b
Hasan Karahan <hasan.karahan81@gmail.com>
2013-02-07 16:28:57 +0500

    Added rST and LaTex report icon usage

--- webed/static/app/mime.js
+++ webed/static/app/mime.js
@@ -26,38 +26,48 @@ var MIME = function () {
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
-    var map2icon = {}, tmp2icon = {
-        'text/plain':'icon-page',
-        'image/*': 'icon-picture',
+    var map2icon = {}, map2icon_cache = {}, tmp2icon = {
         'application/folder': 'icon-folder',
+        'application/project\\+latex': 'icon-report_latex',
+        'application/project\\+rest': 'icon-report_rest',
         'application/project': 'icon-report',
+        'image/*': 'icon-picture',
+        'text/plain':'icon-page',
         '*/*': 'icon-bullet_white',
         '*': 'icon-bullet_white'
-    }
+    };
 
     $.each (tmp2icon, function (key, value) {
         map2icon['^' + key.replace (/\?/g,'.').replace (/\*/g,'.*')] = value;
     });
 
-    /**
-     * TODO: Replace O(n) operation with O(1) to improve performance w.r.t. re-
-     *       gex matching!
-     */
-
     function to_icon (mime, suffix) {
-        for (var key in map2icon) {
-            if (mime.match (key)) return map2icon[key] + (suffix ? suffix:'');
-        } return null;
+        var result = map2icon_cache[mime];
+        if (result) return result;
+
+        var keys = [];
+        for (var m2i_key in map2icon) {
+            if (map2icon.hasOwnProperty (m2i_key) && mime.match (m2i_key)) {
+                keys.push (m2i_key);
+            }
+        }
+
+        var key = keys.sort ().pop ();
+        assert (key);
+        var value = map2icon[key] + (suffix ? suffix : '');
+        assert (value);
+
+        return map2icon_cache[mime] = value;
     }
 
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
-    var map2title = {}, tmp2title = {
-        'text/plain':'text',
-        'image/*': 'image',
+    var map2title = {}, map2title_cache = {}, tmp2title = {
         'application/folder': 'folder',
         'application/project': 'project',
+        'image/*': 'image',
+        'text/plain':'text',
         '*/*': 'document',
         '*': 'document'
     }
@@ -66,15 +76,23 @@ var MIME = function () {
         map2title['^' + key.replace (/\?/g,'.').replace (/\*/g,'.*')] = value;
     });
 
-    /**
-     * TODO: Replace O(n) operation with O(1) to improve performance w.r.t. re-
-     *       gex matching!
-     */
-
     function to_title (mime) {
-        for (var key in map2title) {
-            if (mime.match (key)) return map2title[key];
-        } return null;
+        var result = map2title_cache[mime];
+        if (result) return result;
+
+        var keys = [];
+        for (var m2i_key in map2title) {
+            if (map2title.hasOwnProperty (m2i_key) && mime.match (m2i_key)) {
+                keys.push (m2i_key);
+            }
+        }
+
+        var key = keys.sort ().pop ();
+        assert (key);
+        var value = map2title[key];
+        assert (value);
+
+        return map2title_cache[mime] = value;
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -85,7 +103,10 @@ var MIME = function () {
         is_text: is_text,
         is_image: is_image,
         to_icon: to_icon,
-        to_title: to_title
+        to_title: to_title,
+
+        m2i: function () { return map2icon; },
+        m2i_cache: function () { return map2icon_cache; }
     };
 
     ///////////////////////////////////////////////////////////////////////////
