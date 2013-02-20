485c0e2bb3c2312456467eeb085f300c12573356
Hasan Karahan <hasan.karahan81@gmail.com>
2013-02-20 19:59:50 +0500

    minor

--- webed/test/__init__.py
+++ webed/test/__init__.py
@@ -6,7 +6,6 @@ __author__ = 'hsk81'
 from base import BaseTestCase
 from cache import MemcachedTestCase
 from cache import RedisTestCase
-from io import IoTestCase
 from models import ModelsTestCase
 from models import ExtendedModelsTestCase
 from models import PropertyModelsTestCase
