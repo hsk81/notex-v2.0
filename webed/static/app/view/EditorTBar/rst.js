2c255298f6b7e945035dd811fde0365a5a1dae49
Hasan Karahan <hasan.karahan81@gmail.com>
2013-03-11 19:07:02 +0500

    Set `overflowText` for toolbars

--- webed/static/app/view/EditorTBar/rst.js
+++ webed/static/app/view/EditorTBar/rst.js
@@ -9,27 +9,27 @@ Ext.define ('Webed.view.EditorTBar.rst', {
 
     items: [{
         action: 'undo',
-        defaults: {text: 'Undo'},
+        overflowText: 'Undo',
         iconCls: 'icon-arrow_undo-16',
         tooltip: 'Undo&nbsp;<div class="w-shortcut">[CTRL+Z]</div>'
     },{
         action: 'redo',
-        defaults: {text: 'Redo'},
+        overflowText: 'Redo',
         iconCls: 'icon-arrow_redo-16',
         tooltip: 'Redo&nbsp;<div class="w-shortcut">[SHIFT+CTRL+Z]</div>'
     },'-',{
         action: 'cut',
-        defaults: {text: 'Cut'},
+        overflowText: 'Cut',
         iconCls: 'icon-cut-16',
         tooltip: 'Cut&nbsp;<div class="w-shortcut">[CTRL+X]</div>'
     },{
         action: 'copy',
-        defaults: {text: 'Copy'},
+        overflowText: 'Copy',
         iconCls: 'icon-page_white_copy-16',
         tooltip: 'Copy&nbsp;<div class="w-shortcut">[CTRL+C]</div>'
     },{
         action: 'paste',
-        defaults: {text: 'Paste'},
+        overflowText: 'Paste',
         iconCls: 'icon-paste_plain-16',
         tooltip: 'Paste&nbsp;<div class="w-shortcut">[CTRL+V]</div>'
     }, '-', {
@@ -69,107 +69,107 @@ Ext.define ('Webed.view.EditorTBar.rst', {
     },'-',{
         action: 'toggle-strong',
         iconCls: 'icon-text_bold-16',
-        defaults: {text: 'Strong Emphasis'},
+        overflowText: 'Strong Emphasis',
         tooltip: 'Strong Emphasis&nbsp;<div class="w-shortcut">[CTRL+B]</div>'
     },{
         action: 'toggle-italic',
         iconCls: 'icon-text_italic-16',
-        defaults: {text: 'Emphasis'},
+        overflowText: 'Emphasis',
         tooltip: 'Emphasis&nbsp;<div class="w-shortcut">[CTRL+I]</div>'
     },{
         action: 'toggle-literal',
         iconCls: 'icon-text_allcaps-16',
-        defaults: {text: 'Literal'},
+        overflowText: 'Literal',
         tooltip: 'Literal&nbsp;<div class="w-shortcut">[CTRL+L]</div>'
     },'-',{
         action: 'toggle-subscript',
         iconCls: 'icon-text_subscript-16',
-        defaults: {text: 'Subscript'},
+        overflowText: 'Subscript',
         tooltip: 'Subscript'
     },{
         action: 'toggle-supscript',
         iconCls: 'icon-text_superscript-16',
-        defaults: {text: 'Superscript'},
+        overflowText: 'Superscript',
         tooltip: 'Superscript'
     },'-',{
         action: 'lower-case',
         iconCls: 'icon-text_lowercase-16',
-        defaults: {text: 'Lower Case'},
+        overflowText: 'Lower Case',
         tooltip: 'Lower Case'
     },{
         action: 'upper-case',
         iconCls: 'icon-text_uppercase-16',
-        defaults: {text: 'Upper Case'},
+        overflowText: 'Upper Case',
         tooltip: 'Upper Case'
     },'-',{
         action: 'decrease-indent',
         iconCls: 'icon-text_indent_remove-16',
-        defaults: {text: 'Decrease Indent'},
+        overflowText: 'Decrease Indent',
         tooltip: 'Decrease Indent'
     },{
         action: 'increase-indent',
         iconCls: 'icon-text_indent-16',
-        defaults: {text: 'Increase Indent-16'},
+        overflowText: 'Increase Indent-16',
         tooltip: 'Increase Indent'
     },'-',{
         action: 'toggle-bullet-list',
         iconCls: 'icon-text_list_bullets-16',
-        defaults: {text: 'Bullet List'},
+        overflowText: 'Bullet List',
         tooltip: 'Bullet List'
     },{
         action: 'toggle-number-list',
         iconCls: 'icon-text_list_numbers-16',
-        defaults: {text: 'Number List'},
+        overflowText: 'Number List',
         tooltip: 'Number List'
     },'-',{
         action: 'insert-figure',
         iconCls: 'icon-picture-16',
-        defaults: {text: 'Figure'},
+        overflowText: 'Figure',
         tooltip: 'Figure'
     },{
         action: 'insert-image',
         iconCls: 'icon-image-16',
-        defaults: {text: 'Image'},
+        overflowText: 'Image',
         tooltip: 'Image'
     },{
         action: 'insert-hyperlink',
         iconCls: 'icon-link-16',
-        defaults: {text: 'Hyperlink'},
+        overflowText: 'Hyperlink',
         tooltip: 'Hyperlink'
     },{
         action: 'insert-footnote',
         iconCls: 'icon-text_horizontalrule-16',
-        defaults: {text: 'Footnote'},
+        overflowText: 'Footnote',
         tooltip: 'Footnote'
     },{
         action: 'insert-horizontal-line',
         iconCls: 'icon-hrule-16',
-        defaults: {text: 'Horizontal Line'},
+        overflowText: 'Horizontal Line',
         tooltip: 'Horizontal Line'
     },'-',{
         action: 'find-previous',
         iconCls: 'icon-document_page_previous-16',
-        defaults: {text: 'Find Previous'},
+        overflowText: 'Find Previous',
         tooltip: 'Find Previous&nbsp;<div class="w-shortcut">[SHIFT+CTRL+G]</div>'
     },{
         action: 'find',
         iconCls: 'icon-find-16',
-        defaults: {text: 'Find'},
+        overflowText: 'Find',
         tooltip: 'Find&nbsp;<div class="w-shortcut">[CTRL+F]</div>'
     },{
         action: 'clear-search',
         iconCls: 'icon-stop-16',
-        defaults: {text: 'Clear Search'},
+        overflowText: 'Clear Search',
         tooltip: 'Clear Search'
     },{
         action: 'find-next',
         iconCls: 'icon-document_page_next-16',
-        defaults: {text: 'Find Next'},
+        overflowText: 'Find Next',
         tooltip: 'Find Next&nbsp;<div class="w-shortcut">[CTRL+G]</div>'
     },'-',{
         action: 'replace-all',
         iconCls: 'icon-text_replace-16',
-        defaults: {text: 'Replace All'},
+        overflowText: 'Replace All',
         tooltip: 'Replace All&nbsp;<div class="w-shortcut">[SHIFT+CTRL+R]</div>'
     }],
 
--- webed/static/app/view/EditorTBar/txt.js
+++ webed/static/app/view/EditorTBar/txt.js
@@ -4,73 +4,73 @@ Ext.define ('Webed.view.EditorTBar.txt', {
 
     items: [{
         action: 'undo',
-        defaults: {text: 'Undo'},
+        overflowText: 'Undo',
         iconCls: 'icon-arrow_undo-16',
         tooltip: 'Undo&nbsp;<div class="w-shortcut">[CTRL+Z]</div>'
     },{
         action: 'redo',
-        defaults: {text: 'Redo'},
+        overflowText: 'Redo',
         iconCls: 'icon-arrow_redo-16',
         tooltip: 'Redo&nbsp;<div class="w-shortcut">[SHIFT+CTRL+Z]</div>'
     },'-',{
         action: 'cut',
-        defaults: {text: 'Cut'},
+        overflowText: 'Cut',
         iconCls: 'icon-cut-16',
         tooltip: 'Cut&nbsp;<div class="w-shortcut">[CTRL+X]</div>'
     },{
         action: 'copy',
-        defaults: {text: 'Copy'},
+        overflowText: 'Copy',
         iconCls: 'icon-page_white_copy-16',
         tooltip: 'Copy&nbsp;<div class="w-shortcut">[CTRL+C]</div>'
     },{
         action: 'paste',
-        defaults: {text: 'Paste'},
+        overflowText: 'Paste',
         iconCls: 'icon-paste_plain-16',
         tooltip: 'Paste&nbsp;<div class="w-shortcut">[CTRL+V]</div>'
     },'-',{
         action: 'lower-case',
         iconCls: 'icon-text_lowercase-16',
-        defaults: {text: 'Lower Case'},
+        overflowText: 'Lower Case',
         tooltip: 'Lower Case'
     },{
         action: 'upper-case',
         iconCls: 'icon-text_uppercase-16',
-        defaults: {text: 'Upper Case'},
+        overflowText: 'Upper Case',
         tooltip: 'Upper Case'
     },'-',{
         action: 'decrease-indent',
         iconCls: 'icon-text_indent_remove-16',
-        defaults: {text: 'Decrease Indent'},
+        overflowText: 'Decrease Indent',
         tooltip: 'Decrease Indent'
     },{
         action: 'increase-indent',
         iconCls: 'icon-text_indent-16',
-        defaults: {text: 'Increase Indent-16'},
+        overflowText: 'Increase Indent-16',
         tooltip: 'Increase Indent'
     },'-',{
         action: 'find-previous',
         iconCls: 'icon-document_page_previous-16',
-        defaults: {text: 'Find Previous'},
+        overflowText: 'Find Previous',
         tooltip: 'Find Previous&nbsp;<div class="w-shortcut">[SHIFT+CTRL+G]</div>'
     },{
         action: 'find',
         iconCls: 'icon-find-16',
-        defaults: {text: 'Find'},
+        overflowText: 'Find',
         tooltip: 'Find&nbsp;<div class="w-shortcut">[CTRL+F]</div>'
     },{
         action: 'clear-search',
         iconCls: 'icon-stop-16',
-        defaults: {text: 'Clear Search'},
+        overflowText: 'Clear Search',
         tooltip: 'Clear Search'
     },{
         action: 'find-next',
         iconCls: 'icon-document_page_next-16',
-        defaults: {text: 'Find Next'},
+        overflowText: 'Find Next',
         tooltip: 'Find Next&nbsp;<div class="w-shortcut">[CTRL+G]</div>'
     },'-',{
         action: 'replace-all',
         iconCls: 'icon-text_replace-16',
-        defaults: {text: 'Replace All'},
+        overflowText: 'Replace All',
         tooltip: 'Replace All&nbsp;<div class="w-shortcut">[SHIFT+CTRL+R]</div>'
     }],
 
