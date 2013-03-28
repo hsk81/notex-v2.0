6abab8a0732e5600fa7f6ab9802e1f8d9882dcf9
Hasan Karahan <hasan.karahan81@gmail.com>
2013-03-28 15:03:33 +0500

    minor

--- autoreload.py
+++ autoreload.py
@@ -18,7 +18,13 @@ import subprocess
 ###############################################################################
 
 def file_filter (name):
-    return not name.startswith ('.') and name.endswith ('.py')
+    return not name.startswith ('.') and \
+           not name.endswith ('.css') and \
+           not name.endswith ('.html') and \
+           not name.endswith ('.js') and \
+           not name.endswith ('.log') and \
+           not name.endswith ('.pyc') and \
+           not name.endswith ('.txt')
 
 def file_times (path):
     for top_level in filter (file_filter, os.listdir (path)):
