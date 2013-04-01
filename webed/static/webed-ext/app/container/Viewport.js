1155142a2059f5e95dd7fd915143c5e97ab37f31
Hasan Karahan <hasan.karahan81@gmail.com>
2013-04-01 15:51:26 +0500

    Border optimization in `Viewport`

--- webed/static/webed-ext/app/container/Viewport.js
+++ webed/static/webed-ext/app/container/Viewport.js
@@ -20,6 +20,7 @@ Ext.define ('Webed.container.Viewport', {
             name: 'projects',
             collapsible: true,
             collapsed: true,
+            layout: 'border',
             split: true,
             minWidth: 281,
             flex: 1,
@@ -33,7 +34,6 @@ Ext.define ('Webed.container.Viewport', {
                 tooltip: '<div>Project Tree&nbsp;<div class="w-shortcut">[F9]</div></div>'
             }],
 
-            layout: 'border',
             items: [{
                 region: 'center',
                 xtype: 'node-tree',
@@ -59,6 +59,13 @@ Ext.define ('Webed.container.Viewport', {
             }]
         },{
             region: 'center',
+
+            border: false,
+            style: {
+                borderStyle: 'solid',
+                borderWidth: '0 0 0 1px'
+            },
+
             flex: 4,
             name: 'hbox',
             layout: {type: 'hbox', align: 'stretch'},
