318f2b00af7aed8ec3c511b1e0edb2d4c463fc15
Hasan Karahan <hasan.karahan81@gmail.com>
2013-04-03 10:58:21 +0500

    Use regular GA instead of Doubleclick's

--- webed/templates/analytics.js
+++ webed/templates/analytics.js
@@ -7,7 +7,7 @@ _gaq.push(['_trackPageview']);
     ga.type = 'text/javascript';
     ga.async = true;
     ga.src = ('https:' == document.location.protocol
-        ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+        ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(ga, s);
 })();
