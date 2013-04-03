32b6609ffb4f6fa8d609c6dad7b11ea775960be7
Hasan Karahan <hasan.karahan81@gmail.com>
2013-04-03 16:09:32 +0500

    Use gs.js directly from google for now

--- webed/templates/analytics-body.js
+++ webed/templates/analytics-body.js
@@ -2,9 +2,11 @@ function ga_init () {
     var ga = document.createElement('script');
     ga.type = 'text/javascript';
     ga.async = true;
-    ga.src = ('https:' == document.location.protocol
+ /* ga.src = ('https:' == document.location.protocol
         ? "{{ url_for('static', filename='ga/ga-ssl.js') }}"
         : "{{ url_for('static', filename='ga/ga-www.js') }}");
+ */ ga.src = ('https:' == document.location.protocol
+        ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(ga, s);
 }
