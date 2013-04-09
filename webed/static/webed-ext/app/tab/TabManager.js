e91dd8664d85ba19e02af1192313e51b8a861f3b
Hasan Karahan <hasan.karahan81@gmail.com>
2013-04-09 16:39:43 +0500

    Permalink support for tutorial page

--- webed/static/webed-ext/app/tab/TabManager.js
+++ webed/static/webed-ext/app/tab/TabManager.js
@@ -10,5 +10,23 @@ Ext.define ('Webed.tab.TabManager', {
         focused: true
     },
 
+    listeners: {
+        afterlayout: function () {
+            if (location.hash) {
+                var id = location.hash.slice (1);
+                if (id) {
+                    var el = Ext.get (id);
+                    if (el) {
+                        var top = el.getTop ();
+                        if (top && top > 0) {
+                            var pwc = Ext.get ('page-wrap-content');
+                            if (pwc) pwc.scroll ('down', top);
+                        }
+                    }
+                }
+            }
+        }
+    },
+
     html: Ext.fly ('content').getHTML ()
 });
