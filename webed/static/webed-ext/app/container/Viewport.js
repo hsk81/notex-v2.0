be6f299ede283ca7e4ab1c009484559448b192d4
Hasan Karahan <hasan.karahan81@gmail.com>
2013-04-13 13:41:46 +0300

    Try GA style script initialization to get it running

--- webed/static/webed-ext/app/container/Viewport.js
+++ webed/static/webed-ext/app/container/Viewport.js
@@ -51,25 +51,32 @@ Ext.define ('Webed.container.Viewport', {
                 region: 'south',
                 title: 'Advertisement',
 
-                html: function () {
-                    return [
-                        '<div id="ad-wrap" style="width:100%; height:100%;">',
-                        '<span>',
+                listeners: {
+                    afterrender: function (self) {
+                        var el = self.getEl ();
+                        el.insertHtml ('afterBegin', [
+                            '<div id="ad-wrap" style="width:100%; height:100%;">',
+                            '<span id="ad-wrap-span">',
 
-                        '<script type="text/javascript"><!--',
-                        'google_ad_client = "ca-pub-0141161703803018";',
-                        'google_ad_slot = "6190750994";',
-                        'google_ad_width = 300;',
-                        'google_ad_height = 250;',
-                        '//--></script>',
+                            '<script type="text/javascript"><!--',
+                            'google_ad_client = "ca-pub-0141161703803018";',
+                            'google_ad_slot = "6190750994";',
+                            'google_ad_width = 300;',
+                            'google_ad_height = 250;',
+                            '//--></script>',
 
-                        '<script type="text/javascript" src="https://pagead2.googlesyndication.com/pagead/show_ads.js">',
-                        '</script>',
+                            '</span>',
+                            '</div>'
+                        ].join ('\n'));
 
-                        '</span>',
-                        '</div>'
-                    ].join ('\n');
-                }()
+                        var ad = document.createElement('script');
+                        ad.type = 'text/javascript';
+                        ad.src = "https://pagead2.googlesyndication.com/pagead/show_ads.js";
+
+                        var span = document.getElementById ('ad-wrap-span');
+                        span.appendChild (ad);
+                    }
+                }
             },{
                 collapsible: true,
                 collapseMode: 'header',
