80350c7f8fc5495c24296f50f42f4a0aab79eab4
Hasan Karahan <hasan.karahan81@gmail.com>
2013-04-14 21:27:05 +0300

    minor

--- webed/templates/js/expand-app.js
+++ webed/templates/js/expand-app.js
@@ -1,4 +1,4 @@
-function start_notex (self) {
+function expand_app (self) {
     var viewport = Ext.ComponentQuery.query ('viewport').pop ();
     if (viewport) {
         var projects = viewport.down ('panel[name=projects]');
