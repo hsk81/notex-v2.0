a564005fff04cf78f2391041a12d87b017283212
Hasan Karahan <hasan.karahan81@gmail.com>
2013-04-15 23:26:09 +0300

    Avoid serving GA scripts (just rely on Google)

--- webed/templates/js/analytics-body.js
+++ webed/templates/js/analytics-body.js
@@ -2,11 +2,11 @@ function ga_init () {
     var ga = document.createElement('script');
     ga.type = 'text/javascript';
     ga.async = true;
-    ga.src = ('https:' == document.location.protocol
+ /* ga.src = ('https:' == document.location.protocol
         ? "{% assets 'ga-ssl' %}{{ ASSET_URL }}{% endassets %}"
         : "{% assets 'ga-www' %}{{ ASSET_URL }}{% endassets %}");
- /* ga.src = ('https:' == document.location.protocol
+ */ ga.src = ('https:' == document.location.protocol
         ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
- */ var s = document.getElementsByTagName('script')[0];
+    var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(ga, s);
 }
