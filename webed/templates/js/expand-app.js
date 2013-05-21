834640c032df82898d4f6c6f8b9fe53850abffbd
Hasan Karahan <hasan.karahan81@gmail.com>
2013-05-21 11:31:08 +0300

    JavaScript based expansion is not reliable if the rest of the framework did not load properly (which is possible if we use CloudFlare's *rocket-load* feature); therefore stick to URL based expansion (i.e. start of the application).

--- webed/templates/js/expand-app.js
+++
@@ -1,7 +0,0 @@
-function expand_app (self) {
-    var viewport = Ext.ComponentQuery.query ('viewport').pop ();
-    if (viewport) {
-        var projects = viewport.down ('panel[name=projects]');
-        if (projects) projects.expand ();
-    }
-}
