27ded43ba7df91461140f1b22fa90aa0af98676d
Hasan Karahan <hasan.karahan81@gmail.com>
2013-08-08 12:37:13 +0500

    Issue #35 -- Disable multiple-tld tracking since it's not clear if it's required

--- webed/templates/js/analytics-head.js
+++ webed/templates/js/analytics-head.js
@@ -1,5 +1,4 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-6838945-8']);
 _gaq.push(['_setDomainName', 'notex.ch']);
-_gaq.push(['_setAllowLinker', true]);
 _gaq.push(['_trackPageview']);
