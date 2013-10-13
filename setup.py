fafe9120730cf8a297dabebe714c223a22e02b7f
Hasan Karahan <hasan.karahan81@gmail.com>
2013-10-13 15:40:54 +0300

    Have frozen dependency versions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 ###############################################################################
 ###############################################################################
 
-version = '2.1.2'
+version = '2.1.3'
 
 ###############################################################################
 ###############################################################################
@@ -25,37 +25,56 @@ if __name__ == '__main__':
         include_package_data=True,
         zip_safe=False,
         install_requires=[
-            'acidfs>=1.0',
-            'compressinja>=0.0.2',
-            'Flask>=0.9',
-            'Flask-Admin>=1.0.6',
-            'Flask-Assets>=0.8',
-            'Flask-DebugToolbar>=0.8',
-            'Flask-Login>=0.1.3',
-            'Flask-Mail>=0.7.6',
-            'Flask-Script>=0.5.3',
-            'Flask-SQLAlchemy>=0.16',
-            'Flask-Psycopg2>=1.2',
-            'Werkzeug>=0.8.3',
-            'ipython>=0.13.1',
-            'hiredis>=0.1.1',
-            'pylibmc>=1.2.3',
-            'PyYAML>=3.10',
-            'pyzmq>=13.0.0',
-            'redis>=2.7.2',
-            'requests>=1.2.0',
-            'spritemapper>=1.0.0',
-            'ujson>=1.30',
-            'uWSGI>=1.9.4',
-            'yuicompressor>=2.4.7'
+            'Flask==0.10.1',
+             ## REQUIRES: `pip uninstall Flask-Admin` && `setup.py install`!!
+            'Flask-Admin==1.0.6',
+            'Flask-Assets==0.8',
+            'Flask-DebugToolbar==0.8.0',
+            'Flask-Login==0.2.7',
+            'Flask-Mail==0.9.0',
+            'Flask-Psycopg2==1.2',
+            'Flask-SQLAlchemy==1.0',
+            'Flask-Script==0.5.3',
+            'Flask-WTF==0.9.3',
+            'Jinja2==2.7.1',
+            'MarkupSafe==0.18',
+            'PyYAML==3.10',
+            'SQLAlchemy==0.8.2',
+            'WTForms==1.0.5',
+            'Werkzeug==0.9.4',
+            'acidfs==1.1dev',
+            'blinker==1.3',
+            'compressinja==0.0.2',
+            'hiredis==0.1.1',
+            'ipython==1.1.0',
+            'itsdangerous==0.23',
+            'psycopg2==2.5.1',
+            'pylibmc==1.2.3',
+            'pyzmq==13.1.0',
+            'redis==2.8.0',
+            'requests==2.0.0',
+            'spritemapper==1.0.0',
+            'transaction==1.4.1',
+            'uWSGI==1.9.18.1',
+            'ujson==1.33',
+            'webassets==0.8',
+            'webed==2.1.3',
+            'wsgiref==0.1.2',
+            'yuicompressor==2.4.8',
+            'zope.interface==4.0.5',
         ],
 
         dependency_links=[
-            'http://github.com/hsk81/pylibmc/tarball/master#egg=pylibmc-1.2.3',
-            'http://github.com/hsk81/acidfs/tarball/master#egg=acidfs-1.0',
+            'http://github.com/hsk81/acidfs/tarball/master#egg=acidfs-1.1dev',
             'http://github.com/hsk81/flask-admin/tarball/master#egg=Flask-Admin-1.0.6'
         ]
     )
+
+    print
+    print "# If this is the 1st run, execute also:"
+    print "$ pip uninstall Flask-Admin"
+    print "# And then re-run setup:"
+    print "$ setup.py install"
 
 ###############################################################################
 ###############################################################################
