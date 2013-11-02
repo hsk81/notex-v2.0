dfe6b4aca7aa4d120426175a7d9f51fc918f5630
Hasan Karahan <hasan.karahan81@gmail.com>
2013-11-03 01:09:33 +0200

    GA upgrade

--- webed/templates/js/analytics.js
+++ webed/templates/js/analytics.js
@@ -8,7 +8,7 @@ function ga_init () {
     ga.type = 'text/javascript';
     ga.async = true;
     ga.src = ('https:' == document.location.protocol
-        ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(ga, s);
 }
