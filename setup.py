07c86500fdde3a9d5e425d3b4068b9026f18e6af
Hasan Karahan <hasan.karahan81@gmail.com>
2014-01-05 18:07:27 +0500

    Upgraded dependencies (AFAP)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 ###############################################################################
 ###############################################################################
 
-version = '2.1.14'
+version = '2.1.15'
 
 ###############################################################################
 ###############################################################################
@@ -28,17 +28,17 @@ if __name__ == '__main__':
             'Flask==0.10.1',
             'Flask-Admin==1.0.7',
             'Flask-Assets==0.8',
-            'Flask-DebugToolbar==0.8.0',
-            'Flask-Login==0.2.7',
+            'Flask-DebugToolbar==0.9.0',
+            'Flask-Login==0.2.7', # <= 0.2.9
             'Flask-Mail==0.9.0',
             'Flask-Psycopg2==1.2',
             'Flask-SQLAlchemy==1.0',
-            'Flask-Script==0.5.3',
+            'Flask-Script==0.5.3', # <= 0.6.6
             'Flask-WTF==0.9.4',
             'Jinja2==2.7.1',
             'MarkupSafe==0.18',
             'PyYAML==3.10',
-            'SQLAlchemy==0.8.2',
+            'SQLAlchemy==0.9.0',
             'WTForms==1.0.5',
             'Werkzeug==0.9.4',
             'acidfs==1.1dev',
@@ -49,14 +49,14 @@ if __name__ == '__main__':
             'itsdangerous==0.23',
             'psycopg2==2.5.1',
             'pylibmc==1.2.3',
-            'pyzmq==13.1.0',
-            'redis==2.8.0',
-            'requests==2.0.0',
+            'pyzmq==14.0.1',
+            'redis==2.9.0',
+            'requests==2.1.0',
             'spritemapper==1.0.0',
             'transaction==1.4.1',
-            'uWSGI==1.9.18.1',
+            'uWSGI==2.0',
             'ujson==1.33',
-            'webassets==0.8',
+            'webassets==0.8', # <= 0.9
             'wsgiref==0.1.2',
             'yuicompressor==2.4.8',
             'zope.interface==4.0.5',
